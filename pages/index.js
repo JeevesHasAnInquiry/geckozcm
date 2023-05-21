53db2fce38929bc45267c09e79c84110987e6ac0
Jeeves <kleanplateklub@gmail.com>
2023-05-21 05:32:19 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,96 @@
+import styles from "../styles/Home.module.css";
+import { useMemo, useState } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { MetaplexProvider } from "./MetaplexProvider";
+import { MintNFTs } from "./MintNFTs";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import dynamic from 'next/dynamic';
+import geckozLogo from '../public/images/gg_logo.png'
+
+import Image from 'next/image';
+
+export default function Home() {
+  const [network, setNetwork] = useState(WalletAdapterNetwork.Devnet);
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  const handleChange = (event) => {
+    switch (event.target.value) {
+      case "devnet":
+        setNetwork(WalletAdapterNetwork.Devnet);
+        break;
+      case "mainnet":
+        setNetwork(WalletAdapterNetwork.Mainnet);
+        break;
+      case "testnet":
+        setNetwork(WalletAdapterNetwork.Testnet);
+        break;
+      default:
+        setNetwork(WalletAdapterNetwork.Devnet);
+        break;
+    }
+  };
+
+  const ButtonWrapper = dynamic(() =>
+    import('@solana/wallet-adapter-react-ui', { ssr: false }).then((mod) => mod.WalletMultiButton)
+  );
+
+  return (
+    <div>
+      <div className={styles.geckozContainer}>
+        <div className={styles.inlineContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={geckozLogo} alt="Greedy Geckoz"/>
+          </div>
+          {/* <div className={styles.simpleContainer}>
+            <div className={styles.inlineContainer}>
+              <div className={styles.largerTitle}>Metaplex CandyMachine V3 UI</div>
+            </div>
+            <div className={styles.inlineContainer}>
+            </div>
+
+          </div> */}
+        </div>
+      </div>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <MetaplexProvider>
+              <div className={styles.App}>
+                <ButtonWrapper />
+                <MintNFTs onClusterChange={handleChange} />
+              </div>
+            </MetaplexProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
+}
